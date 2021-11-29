638242a4668d98814c170b0b24abd76312af4463
Léa Renault <84766057+Learenault@users.noreply.github.com>
2021-11-29 18:26:06 +0100

    avancement

--- script.js
+++ script.js
@@ -7,36 +7,33 @@ let date = document.getElementById("date");
 let heure = document.getElementById("heure");
 let destination = document.getElementById('destination');
 let resultatsAPI;
-const tableau = ["Rains","Clear","Clouds","neige"];
 const tab_jour = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"];
 const tab_mois= ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 
  appelApi = () => {
      if(app.classList.contains(temps.innerHTML)){
         app.classList.remove(temps.innerHTML);
-     }
-let ville = document.getElementById('searchInput').value
+    }
+let ville = document.getElementById('searchInput').value;
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ville}&units=metric&appid=ec0b856f18a13865681b046eea95030b`)
   .then((reponse) => {
-      return reponse.json();
-  })
+      return reponse.json();})
   .then((data)=>{
-      console.log(data);
-      resultatsAPI = data;
-      destination.innerHTML = resultatsAPI.name + ", "+ resultatsAPI.sys.country
-      temps.innerText = resultatsAPI.weather[0].main;
-      degres.innerText = resultatsAPI.main.temp + "°";
-      app.classList.add(temps.innerHTML);
-
+      if(data.cod == "404"){
+        return alert("Merci de renseigner une destination valide");
+        }
+    resultatsAPI = data;
+    destination.innerHTML = resultatsAPI.name + ", "+ resultatsAPI.sys.country
+    temps.innerText = resultatsAPI.weather[0].main;
+    degres.innerText = resultatsAPI.main.temp + "°";
+    app.classList.add(temps.innerHTML);
 })
 };
  
 UpdateDate = () => {
     let dates = new Date();
-date.innerHTML = tab_jour[dates.getDay()]+ " "+ dates.getDate()+ " " + tab_mois[dates.getMonth()] + " " + dates.getFullYear();
-heure.innerHTML = "à " + dates.getHours() + "h " + dates.getMinutes(); 
-
-
+    date.innerHTML = tab_jour[dates.getDay()]+ " "+ dates.getDate()+ " " + tab_mois[dates.getMonth()] + " " + dates.getFullYear();
+    heure.innerHTML = "à " + dates.getHours() + "h " + dates.getMinutes(); 
 }
 
 submit.addEventListener("click", () => {
